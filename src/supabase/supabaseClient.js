0d274168b58851e6ac38503d4765cbb4a389f520
leeboa2005 <leeboa2005@naver.com>
2024-06-24 10:43:18 +0900

    remove : API  테스트 코드 삭제

--- src/supabase/supabaseClient.js
+++ src/supabase/supabaseClient.js
@@ -3,13 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 const VITE_SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const VITE_SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 
-console.log('Supabase URL:', VITE_SUPABASE_URL);
-console.log('Supabase Key:', VITE_SUPABASE_KEY);
-
-if (!VITE_SUPABASE_URL || !VITE_SUPABASE_KEY) {
-  throw new Error('Supabase URL and Key must be provided.');
-}
-
 const supabaseClient = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_KEY);
 
 export default supabaseClient;
